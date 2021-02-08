4e69a7a2900cf9361bd682b4b37c54f9d0fc376c
dorahero <66933004+dorahero@users.noreply.github.com>
2021-02-08 15:18:32 +0800

    49-Group-Anagrams

---
+++ create-new.bat
@@ -0,0 +1,3 @@
+mkdir Algorithms\Medium\%1%
+copy Algorithms\Medium\49-Group-Anagrams\GroupAnagrams.py Algorithms\Medium\%1\%1.py
+copy Algorithms\Medium\49-Group-Anagrams\README.md Algorithms\Medium\%1\README.md
